14031a507bf6aa86bfcad97f7c47ae0072425578
Yaqueline Hoyos <yaqueline.hoyos@udea.edu.co>
2023-12-21 21:57:53 +0100

    Website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import './fonts/Josefin_Sans/JosefinSans-VariableFont_wght.ttf';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="first-half">
+          <div className="first-half_header">
+            <div className="first-half_header_line"></div>
+            <div className="first-half_header_title">Seite im Aufbau...</div>
+          </div>
+          <div className="first-half_info">
+            Verena <span className="first-half_info_and">&</span> Alexander
+          </div>
+          <div className="first-half_comming-soon">Wir feiern am 29. Juni 2024. Du bist hier richtig. Wir genießen gerade die Feiertage und arbeiten daran euch alle Informationen zur Hochzeit bereit zu stellen. Bis dahin wünschen wir euch frohe Weihnachten</div>
+        </div>
+        {/* <div className="second-half">Photos are here</div> */}
       </header>
     </div>
   );
