0eb49e13c8ab07a4e61b2b6f666e11d6d367dce1
Yaqueline Hoyos <yaqueline.hoyos@udea.edu.co>
2023-12-22 14:32:19 +0100

    chnage text

--- src/App.js
+++ src/App.js
@@ -13,9 +13,13 @@ function App() {
           <div className="first-half_info">
             Verena <span className="first-half_info_and">&</span> Alexander
           </div>
-          <div className="first-half_comming-soon">Wir feiern am 29. Juni 2024. Du bist hier richtig. Wir genießen gerade die Feiertage und arbeiten daran euch alle Informationen zur Hochzeit bereit zu stellen. Bis dahin wünschen wir euch frohe Weihnachten</div>
+          <div className="first-half_comming-soon">
+            <p>Ihr Lieben,</p>
+            <p>am 29. Juni 2024 wollen wir mit euch in der <a className="webpage-link" href='https://www.alte-turnhalle-berlin.de/' target="_blank" rel="noreferrer">Alten Turnhalle</a> in Berlin Friedrichshain unsere Hochzeit feiern.</p>
+            <p>Auf dieser Website werdet ihr schon ganz bald alle Informationen zur Feier, zu den Übernachtungsmöglichkeiten und die Möglichkeit zur Zu-und Absage finden. Aktuell arbeiten wir noch daran, alles wichtige zusammenzutragen und für euch hier zur Verfügung zu stellen. Schaut einfach die Tage nochmal hier vorbei.</p>
+            <p>Bis dahin wünschen wir euch frohe Weihnachten und einen guten Start ins neue Jahr!</p>
+          </div>
         </div>
-        {/* <div className="second-half">Photos are here</div> */}
       </header>
     </div>
   );
